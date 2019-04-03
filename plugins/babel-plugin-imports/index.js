2fe68505af220ba7d867fa824dd94c67b1136d9d
sanjunzou <zousanjun@360.net>
2019-04-03 13:50:22 +0800

    enter与exit

--- plugins/babel-plugin-imports/index.js
+++ plugins/babel-plugin-imports/index.js
@@ -4,16 +4,21 @@ const types = require('@babel/types');
 module.exports = function ({ types }) {
     return {
         visitor:  {
-            ImportDeclaration(path, ref={}) {
-                let { opts } = ref //  { library: 'lodash' }
-                let node = path.node;
-                let specifiers = node.specifiers
-                if (node.type === 'ImportDeclaration' && opts.library === node.source.value && !types.isImportDeclaration(specifiers[0])) {
-                    let newImports = specifiers.map((specifier) => {
-                        // import {cloneDeep} from 'lodash' => import cloneDeep from 'lodash/cloneDeep'
-                        return types.importDeclaration([types.ImportDefaultSpecifier(specifier.local)], types.stringLiteral(`${node.source.value}/${specifier.local.name}`))
-                    });
-                    path.replaceWithMultiple(newImports)
+            ImportDeclaration: {
+                enter(path, ref={}) {
+                    let { opts } = ref //  { library: 'lodash' }
+                    let node = path.node;
+                    let specifiers = node.specifiers
+                    if (node.type === 'ImportDeclaration' && opts.library === node.source.value && !types.isImportDeclaration(specifiers[0])) {
+                        let newImports = specifiers.map((specifier) => {
+                            // import {cloneDeep} from 'lodash' => import cloneDeep from 'lodash/cloneDeep'
+                            return types.importDeclaration([types.ImportDefaultSpecifier(specifier.local)], types.stringLiteral(`${node.source.value}/${specifier.local.name}`))
+                        });
+                        path.replaceWithMultiple(newImports)
+                    }
+                },
+                exit(...args) {
+                    console.log('exit ', args)
                 }
             }
         }
